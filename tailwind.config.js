9d450ed688c256d7f8c04d58f25c1065ff295b32
Mayankmishra110 <kmmay15@gmail.com>
2024-03-18 23:26:29 +0530

    front-end setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Includes HTML templates and TypeScript component files
+    "./src/**/*.component.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
